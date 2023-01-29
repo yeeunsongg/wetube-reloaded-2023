172353ff00792c35a3357d9a82daf5ab898ced66
yeeunsongg <dorothi1002@naver.com>
2023-01-29 18:20:33 +0900

    230129 middleware

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+};
+
+app.get("/", logger, handleHome);
+
+const handlelistening = () =>
+  console.log(`✔️ Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handlelistening);
